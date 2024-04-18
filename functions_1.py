f36ffd4109f1176aa8ae11dedeecc23b5db0883a
Jose Venegas <jo.ve.or.84@gmail.com>
2024-04-17 18:20:20 -0600

    Added more functions

--- functions_1.py
+++ functions_1.py
@@ -98,3 +98,11 @@ def count_vowel(phrase):
 count_vowel("Hello")
 
 
+def divisible_by_ten(nums):
+    counter = 0
+    for num in nums:
+        if num % 10 == 0:
+            counter += 1
+    return counter
+        
+print(divisible_by_ten([20, 25, 30, 35, 40]))
