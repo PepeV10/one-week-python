23633419e82880d7567c1d0a3d14643f8e387ab7
Jose Venegas <jo.ve.or.84@gmail.com>
2024-04-21 09:34:55 -0600

    Added another challenge

--- loops_advanced.py
+++ loops_advanced.py
@@ -138,3 +138,47 @@ def max_num(nums):
 
 There are a few different ways to accomplish this task, but the way we did it was to check every element in the list and see if there is one bigger than what we currently think is the biggest. If there is a bigger one, then replace it. We keep replacing the number until the largest number has been found.
 """
+
+"""
+4. Same Values
+In this challenge, we need to find the indices in two equally sized lists where the numbers match. We will be iterating through both of them at the same time and comparing the values, if the numbers are equal, then we record the index. These are the steps we need to accomplish this:
+
+Define our function to accept two lists of numbers
+Create a new list to store our matching indices
+Loop through each index to the end of either of our lists
+Within the loop, check if our first list at the current index is equal to the second list at the current index. If so, append the index where they matched
+Return our list of indices
+Coding question
+Questions
+Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+
+The function should return a list of the indices where the values were equal in lst1 and lst2.
+
+For example, the following code should return [0, 2, 3]
+
+same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])
+"""
+#Write your function here
+def same_values(lst1, lst2):
+    matching_indices = []
+    for match in range(len(lst1)):
+        if lst1[match] == lst2[match]:
+            matching_indices.append(match)
+    return matching_indices
+        
+#Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+"""
+Here’s how we did it:
+
+def same_values(lst1, lst2):
+  new_lst = []
+  for index in range(len(lst1)):
+    if lst1[index] == lst2[index]:
+      new_lst.append(index)
+  return new_lst
+
+In this solution, we used a loop that iterates using the range of the len of our list. This generates the indices we need to iterate through. Note that we assume the lists are of equal size. We then access the element at the current index from each list using lst1[index] and lst2[index]. If they are equal we add the index to the new list. Finally, we return the results.
+"""
+
