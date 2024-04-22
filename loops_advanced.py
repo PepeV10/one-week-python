b74dfc6f25a7e1e09b78e37fd83e2e8c629f837d
Jose Venegas <jo.ve.or.84@gmail.com>
2024-04-22 07:31:03 -0600

    Finished the last Challenge

--- loops_advanced.py
+++ loops_advanced.py
@@ -182,3 +182,46 @@ def same_values(lst1, lst2):
 In this solution, we used a loop that iterates using the range of the len of our list. This generates the indices we need to iterate through. Note that we assume the lists are of equal size. We then access the element at the current index from each list using lst1[index] and lst2[index]. If they are equal we add the index to the new list. Finally, we return the results.
 """
 
+
+"""
+5. Reversed List
+For the final challenge, we are going to test two lists to see if the second list is the reverse of the first list. There are a few different ways to approach this, but we are going to try a method that iterates through each of the values in one direction for the first list and compares them against the values starting from the other direction in the second list. Here is what you need to do:
+
+Define a function that has two input parameters for our lists
+Loop through every index in one of the lists from beginning to end
+Within the loop, compare the element in the first list at the current index against the element at the second list’s last index minus the current index. If there was a mismatch, then the lists aren’t reversed and we can return False
+If the loop ended successfully, then we know the lists are reversed and we can return True.
+Coding question
+Questions
+Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
+
+The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+
+For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+"""
+#Write your function here
+def reversed_list(lst1, lst2):
+    for match in range(len(lst1)):
+        if lst1[match] != lst2[len(lst2) - 1 - match]:
+            return False
+    return True
+
+#Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
+
+"""
+Here is one way to do it:
+
+def reversed_list(lst1, lst2):
+  for index in range(len(lst1)):
+    if lst1[index] != lst2[len(lst2) - 1 - index]:
+      return False
+  return True
+
+In this code, we iterate through each of the indices for the entire length of either of the lists (since we assume the lengths are equal) and we perform a comparison on each of the elements. We get the element at the current index from our first list with lst1[index] and we test it against the last index of the second list minus the current index len(lst2) - 1 – index.
+
+That math is a little complicated — it helps to look at a concrete example. If we are given a list of 5 elements, the valid indices are 0 to 4. Because of this, the last index in the second list is len(lst2) - 1, or 5 - 1 = 4. Now in order to get the inverse of the position we are at in the first list, we subtract the index we are at from the end of the second list. So on the first pass, we’ll compare the element at position 0 to the element at position 5 - 1 - 0 = 4. On the next pass, we’ll compare the element at position 1 to the element at position 5 - 1 - 1 = 3, and so on.
+
+If any of the two elements are not equal then we know that the second list is not the reverse of the first list and we return False. If we made it to the end without a mismatch then we can return True since the second list is the reverse of the first. You could also try simplifying this code by using the python function reversed() or other methods that you will learn later on such as ‘slicing’.
+"""
